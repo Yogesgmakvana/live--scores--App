1ac42ac0a07a72a570c2d69fc87ed89008398e4c
Yogesh Makwana <makwanay324@gmail.com>
2026-06-04 12:22:56 +0530

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:live_scores/screens/auth/register_screen.dart';
 import 'package:live_scores/screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:live_scores/splash_screen.dart';
-import 'firebase_options.dart';
+import 'package:live_scores/screens/splash_screen.dart';
+import 'firebase/firebase_options.dart';
 
 void main()async {
    WidgetsFlutterBinding.ensureInitialized();
